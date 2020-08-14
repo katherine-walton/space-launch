0458a681a3ffada6e2a2ec4e5ad1aa49133d7947
katherine-walton <katherinefaithwalton@gmail.com>
2020-08-13 21:48:13 -0700

    Print agency on cards test

--- scripts.js
+++ scripts.js
@@ -10,22 +10,26 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+// request.open('GET', 'https://launchlibrary.net/1.3/agency/NASA', true);
+request.open('GET', 'https://launchlibrary.net/1.3/agency', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.agencies.forEach(agency => {
+      console.log(agency);
+
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = agency.name;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      agency.countryCode = agency.countryCode.substring(0, 300);
+      p.textContent = `${agency.countryCode}`;
 
       container.appendChild(card);
       card.appendChild(h1);
@@ -34,7 +38,7 @@ request.onload = function () {
   } else {
     const errorMessage = document.createElement('marquee');
     errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
+    app.appendChild(errorMessage); 
   }
 }
 
